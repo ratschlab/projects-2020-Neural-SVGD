Refactored KSD and SVGD; fixed lots of bugs. * Added stein.py for new implementation of ksd and svgd update computation * better error messages everywhere * KSD computation and SVGD update computation had similar bug. Fixed now; this means working with more particles works better now. * new helper funcs for plotting, etc * removed "two track" in SVGD.train()

metrics.py
@@ -7,6 +7,7 @@ import numpy as onp
 
 import svgd
 import utils
+import stein
 
 # distributions packaged with metrics
 # check wikipedia for computation of higher moments https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Higher_moments
@@ -14,7 +15,12 @@ import utils
 class Distribution():
     """Base class for package logpdf + metrics + sampling"""
     def __init__(self):
+        self.expectations = None
+        self.sample_metrics = None
+        self.d = None
         pass
+
+    key = random.PRNGKey(0)
 
     def newkey(self):
         self.key = random.split(self.key)[0]
@@ -25,20 +31,29 @@ class Distribution():
     def logpdf(self, x):
         raise NotImplementedError()
 
-    def compute_metrics(self, x):
-        """Compute metrics given samples"""
-        sample_expectations = [np.mean(value, axis=0) for value in (x, x**2, np.cos(x), np.sin(x))]
-        square_errors = [(sample - true)**2 for sample, true in zip(sample_expectations, self.expectations)]
-        square_errors = np.array(square_errors) # shape (4, d)
+    def compute_metrics(self, x, normalize=False):
+        """Compute metrics given samples.
+        If normalize = True, then all values are divided by the corresponding expected value for a true random sample of the same size."""
+        if x.shape[-1] != self.d:
+            raise ValueError(f"Particles x need to have shape (n, d), where d = {self.d} is the particle dimension.")
+        if normalize:
+            n = x.shape[0]
+            metrics = self.compute_metrics(x)
+            sample_metrics = self.compute_metrics_for_sample(n)
+            return utils.dict_divide(metrics, sample_metrics)
+        else:
+            sample_expectations = [np.mean(value, axis=0) for value in (x, x**2, np.cos(x), np.sin(x))]
+            square_errors = [(sample - true)**2 for sample, true in zip(sample_expectations, self.expectations)]
+            square_errors = np.array(square_errors) # shape (4, d)
 
-        ksds = [ksd(x, self.logpdf, h) for h in (0.1, 1, 10)]
-        ksds = np.array(ksds)
+            ksds = [ksd(x, self.logpdf, h) for h in (0.1, 1, 10)]
+            ksds = np.array(ksds)
 
-        metrics_dict = {
-            "square_errors": square_errors, # shape (4, d)
-            "ksds": ksds # shape (3,)
-        }
-        return metrics_dict
+            metrics_dict = {
+                "square_errors": square_errors, # shape (4, d)
+                "ksds": ksds # shape (3,)
+            }
+            return metrics_dict
 
     def get_metrics_shape(self):
         return {
@@ -51,6 +66,15 @@ class Distribution():
         "ksds": [f"KSD {h}" for h in (0.1, 1, 10)]
     }
 
+    def compute_metrics_for_sample(self, sample_size):
+        """For benchmarking. Returns metrics computed for a true random sample of size sample_size, averaged over 100 random seeds."""
+        if sample_size not in self.sample_metrics:
+            def compute():
+                sample = self.sample(shape=(sample_size,))
+                sample = np.reshape(sample, newshape=(sample_size, self.d))
+                return self.compute_metrics(sample)
+            self.sample_metrics[sample_size] = utils.dict_mean([compute() for _ in range(100)])
+        return self.sample_metrics[sample_size]
 
 class Gaussian(Distribution):
     def __init__(self, mean, cov):
@@ -77,25 +101,39 @@ class Gaussian(Distribution):
         char = np.exp(np.vdot(t, (1j * mean - np.dot(cov, t) / 2)))
         self.expectations = [mean, np.diagonal(cov) + mean**2, np.real(char), np.imag(char)]
         self.key = random.PRNGKey(0)
+        self.sample_metrics = dict()
         return None
 
     def sample(self, shape):
         self.newkey()
-        return random.multivariate_normal(self.key, self.mean, self.cov, shape)
+        out = random.multivariate_normal(self.key, self.mean, self.cov, shape)
+        self.newkey()
+        return out.reshape(shape + (self.d,)) if self.d > 1 else out.reshape(shape)
+
+    def _checkx(self, x):
+        """check if particle (single particle shape (d,)) in right shape, etc"""
+        x = np.array(x)
+        if x.ndim == 0 and self.d == 1:
+            x = x.reshape((1,))
+        if x.shape != (self.d,):
+            raise ValueError(f"x needs to have shape ({self.d},). Instead, received x of shape {x.shape}.")
+        return x
 
     def logpdf(self, x):
-        x = np.array(x)
-        assert x.ndim == 1 or (x.ndim == 0 and self.d == 1)
+        x = self._checkx(x)
         return stats.multivariate_normal.logpdf(x, self.mean, self.cov)
 
     def pdf(self, x):
-        x = np.array(x)
-        assert x.ndim == 1 or (x.ndim == 0 and self.d == 1)
+        x = self._checkx(x)
         return stats.multivariate_normal.pdf(x, self.mean, self.cov)
 
 class GaussianMixture(Distribution):
     def __init__(self, means, covs, weights):
-        """means, covs are np arrays or lists of length k, with entries of shape (d,) and (d, d) respectively.
+        """
+        Arguments:
+        means, covs are np arrays or lists of length k, with entries of shape (d,) and (d, d) respectively. (e.g. covs can be array of shape (k, d, d))
+
+        Initialization:
         self.expectations is a list of expected values of the following expressions: x, x^2, cos(x), sin(x)"""
         means = np.array(means)
         covs = np.array(covs)
@@ -126,6 +164,8 @@ class GaussianMixture(Distribution):
         mean = np.einsum("i,id->d", weights, means)
         xsquares = [np.diagonal(cov) + mean**2 for mean, cov in zip(means, covs)]
         self.expectations = [mean, np.einsum("i,id->d", weights, xsquares), np.real(char), np.imag(char)]
+        self.expectations = [np.squeeze(e) for e in self.expectations]
+
         self.key = random.PRNGKey(0)
         self.means = means
         self.covs = covs
@@ -136,6 +176,7 @@ class GaussianMixture(Distribution):
         # recall Cov(X) = E[XX^T] - mu mu^T = sum_over_components(Cov(Xi) + mui mui^T) - mu mu^T
         mumut = np.einsum("ki,kj->kij", means, means) # shape (k, d, d)
         self.cov = np.average(covs + mumut, weights=weights, axis=0) - np.outer(mean, mean)
+        self.sample_metrics = dict()
 
     def sample(self, shape):
         """mutates self.rkey"""
@@ -148,6 +189,7 @@ class GaussianMixture(Distribution):
         out = [sample_from_component(key, c, num) for key, c, num in zip(random.split(self.key, self.num_components), range(self.num_components), counts)]
         out = np.concatenate(out)
         self.newkey()
+
         return out.reshape(shape + (self.d,)) if self.d > 1 else out.reshape(shape)
 
     def _logpdf(self, x):
@@ -180,7 +222,7 @@ class GaussianMixture(Distribution):
 
 ######################################
 # Kernelized Stein Discrepancy
-def ksd(x, logp, bandwidth):
+def _ksd(x, logp, bandwidth):
     """
     Arguments:
     * x: np.array of shape (n, d)
@@ -193,7 +235,18 @@ def ksd(x, logp, bandwidth):
         dphi_dxi_transposed = lambda xi: np.trace(jacfwd(svgd.phistar_i)(xi, x, logp, bandwidth))
         return np.vdot(grad(logp)(xi), svgd.phistar_i(xi, x, logp, bandwidth)) + dphi_dxi_transposed(xi)
 
-    return np.mean(vmap(ksd_i, (0, None, None, None))(x, x, logp, bandwidth))
+    return np.mean(vmap(ksd_i, (0, None, None, None))(x, x, logp, bandwidth)) / x.shape[0]
+
+
+def ksd(xs, logp, bandwidth):
+    kernel = lambda x, y: utils.ard(x, y, bandwidth)
+    def phistar(z):
+        """z has shape (d,).
+        Returns array of shape (d,)."""
+        k = lambda x: kernel(x, z)
+        return stein.stein(k, xs, logp)
+    return stein.stein(phistar, xs, logp, transposed=True)
+
 ksd = jit(ksd, static_argnums=1)
 
 ########################

plot.py
@@ -10,7 +10,8 @@ from jax import vmap
 import metrics
 
 ## plotting utilities
-def equalize_axes(ax):
+def equalize_xy_axes(ax):
+    """input: matplotlib axis object. sets x and y axis to same limits (and returns new limits)."""
     ylim = ax.get_ylim()
     xlim = ax.get_xlim()
 
@@ -19,7 +20,25 @@ def equalize_axes(ax):
     ax.set_xlim(lim)
     return lim
 
+def equalize_axes(axs):
+    """Argument: list of matplotlib axis objects. Sets x resp. y intervals to the maximum along the list."""
+    def transpose_min_max(lims):
+        lims = list(zip(*lims))
+        lim = (min(lims[0]), max(lims[1]))
+        return lim
+    xlims = [ax.get_xlim() for ax in axs]
+    ylims = [ax.get_ylim() for ax in axs]
+    xlim = transpose_min_max(xlims)
+    ylim = transpose_min_max(ylims)
+
+    for ax in axs:
+        ax.set_ylim(ylim)
+        ax.set_xlim(xlim)
+    return xlim, ylim
+
 def bivariate_hist(xout):
+    """argument: np.array of shape (n, 2).
+    plots one scatterplot and two heatmaps of the data in 2 dimensions."""
     def myplot(x, y, s, bins=1000):
         heatmap, xlim, ylim = onp.histogram2d(x, y, bins=bins)
         heatmap = gaussian_filter(heatmap, sigma=s)
@@ -43,12 +62,11 @@ def bivariate_hist(xout):
         if s == 0:
             ax.plot(x, y, 'k.', markersize=5)
             ax.set_title("Scatter plot")
-            equalize_axes(ax)
         else:
             img, extent = myplot(x, y, s)
             ax.imshow(img, extent=extent, origin='lower', cmap=cm.jet)
             ax.set_title("Smoothing with  $\sigma$ = %d" % s)
-        equalize_axes(ax)
+        equalize_xy_axes(ax)
 #        ax.set_aspect("equal")
     plt.show()
 
@@ -108,20 +126,28 @@ def svgd_log(log, style="-", full=False):
 
 def make_meshgrid(func, lims, num=100):
     """
+    Utility to help with plotting a 2d distribution in a 3d plot.
     Arguments:
-    * func: callable. Takes an np.array of shape (d,) as only input, and outputs a scalar.
+    * func: callable. Takes an np.array of shape (2,) as only input, and outputs a scalar.
+    * lims
+    * num
+    Returns:
+    meshgrids xx, yy, f(xx, yy)
     """
     x = y = np.linspace(*lims, num)
     xx, yy = np.meshgrid(x, y)
 
     grid = np.stack([xx, yy]) # shape (2, r, r)
-    zz = vmap(vmap(func, 1), 2)(grid)
+    zz = vmap(vmap(func, 1), 1)(grid)
 
     #zz = np.exp(zz)
     return xx, yy, zz
 
 
 def plot_3d(x, y, z):
+    """makes a 3d plot.
+    Arguments:
+    * x, y, z are np.arrays of shape (k, k). meant to be output of make_meshgrid."""
     fig = plt.figure(figsize=(12, 6))
     ax = fig.gca(projection='3d')
     ax.plot_surface(x, y, z,
@@ -130,16 +156,41 @@ def plot_3d(x, y, z):
                   antialiased=True)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
-    ax.set_zlabel('z');
+    ax.set_zlabel('z')
     plt.show()
     if ax is None: print("huh?")
     return ax
 
-def plot_pdf(pdf, lims):
-    return plot_3d(*make_meshgrid(pdf, lims, num=150))
+def plot_pdf(pdf, lims, type="3d"):
+    """
+    Arguments
+    * pdf: callable, computes a distribution on R2.
+    * lims: list of two floats (limits)
+    * type: string, one of "3d", "contour".
+    """
+    if type=="3d":
+        return plot_3d(*make_meshgrid(pdf, lims, num=150))
+    elif type=="contour":
+        return plt.contour(*make_meshgrid(pdf, lims, num=150))
+    else:
+        raise ValueError("type must be one of '3d' or 'contour'.")
 
 
-def make_bar_chart(data, labels=None, figax=None):
+def autolabel_bar_chart(ax, rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        if height < 0.01 or height > 10e4:
+            ann = "{:.2E}".format(height)
+        else:
+            ann = '{:.2f}'.format(height)
+        ax.annotate(ann,
+                    xy = (rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+def make_paired_bar_chart(data, labels=None, figax=None):
     """
     Arguments:
     * data: np.array of shape (2, d), where d is the number of pairs of values.
@@ -156,11 +207,10 @@ def make_bar_chart(data, labels=None, figax=None):
     elif data.ndim == 2:
         d = data.shape[1]
     else:
-        raise ValueError("data must can't have more than 2 dimensions.")
+        raise ValueError("Data must can't have more than 2 dimensions.")
 
     x = np.arange(d)  # the label locations
     width = 0.35  # the width of the bars
-
 
     if figax is None:
         fig, ax = plt.subplots()
@@ -180,25 +230,26 @@ def make_bar_chart(data, labels=None, figax=None):
     if d == 1:
         ax.set_xlim(-1, 2)
 
-    def autolabel(rects):
-        """Attach a text label above each bar in *rects*, displaying its height."""
-        for rect in rects:
-            height = rect.get_height()
-            if height < 0.01 or height > 10e4:
-                ann = "{:.2E}".format(height)
-            else:
-                ann = '{:.2f}'.format(height)
-            ax.annotate(ann,
-                        xy = (rect.get_x() + rect.get_width() / 2, height),
-                        xytext=(0, 3),  # 3 points vertical offset
-                        textcoords="offset points",
-                        ha='center', va='bottom')
 
     ax.legend()
-    autolabel(rects1)
-    autolabel(rects2)
+    autolabel_bar_chart(ax, rects1)
+    autolabel_bar_chart(ax, rects2)
 
     fig.tight_layout()
 
     if figax is None:
         return fig, ax
+
+    return rects1, rects2
+
+def set_axhlines(ax, ys):
+    """Paint len(ys) segmented horizontal lines onto plot given by ax."""
+    try:
+        ns = len(ys)
+    except TypeError:
+        ys = [ys]
+        ns = len(ys)
+    grid = np.linspace(0, 1, num=ns+1)
+
+    for i, y in enumerate(ys):
+        ax.axhline(y, xmin=grid[i], xmax=grid[i+1], color="k", linestyle="--", linewidth=2.5)

svgd.py
@@ -10,13 +10,40 @@ import warnings
 
 import utils
 import metrics
+import stein
 
 def phistar_j(x, y, logp, bandwidth):
-    """Individual summand needed to compute phi^*. That is, phistar_i = \sum_j phistar_j(xj, xi, logp, bandwidth)"""
+    """Individual summand needed to compute phi^*. That is, phistar_i = \sum_j phistar_j(xj, xi, logp, bandwidth).
+    Arguments:
+    * x, y: np.array of shape (d,) or scalar (if d=1)
+    * logp: callable
+    * bandwidth: scalar or np.array of shape (d,)
+    Returns: np.array of shape (d,) or scalar; same shape as x and y
+    """
+    if x.shape != y.shape:
+        raise ValueError("Shapes of particles x and y need to match.")
     kernel = lambda x, y: utils.ard(x, y, bandwidth)
     return grad(logp)(x) * kernel(x, y) + grad(kernel)(x, y)
 
 def phistar_i(xi, x, logp, bandwidth):
+    """
+    Arguments:
+    * xi: np.array of shape (d,), usually a row element of x
+    * x: np.array of shape (n, d)
+    * logp: callable
+    * bandwidth: scalar or np.array of shape (d,)
+
+    Returns:
+    * \phi^*(xi) estimated using the particles x
+    """
+    if xi.ndim > 1:
+        raise ValueError(f"Shape of xi must be (d,). Instead, received shape {xi.shape}")
+    else:
+        pass
+    k = lambda y: utils.ard(y, xi, bandwidth)
+    return stein.stein(k, x, logp)
+
+def _phistar_i(xi, x, logp, bandwidth):
     """
     Arguments:
     * xi: np.array of shape (1, d), usually a row element of x
@@ -37,7 +64,7 @@ def phistar_i(xi, x, logp, bandwidth):
 
     n = x.shape[0]
     xi_rep = np.repeat(xi, n, axis=0)
-    return np.sum(vmap(phistar_j, (0, 0, None, None))(x, xi_rep, logp, bandwidth), axis=0)
+    return np.sum(vmap(phistar_j, (0, 0, None, None))(x, xi_rep, logp, bandwidth), axis=0) # Original state. TODO check what is true and what not.
 
 def phistar(x, logp, bandwidth, xest=None):
     """
@@ -82,13 +109,14 @@ def median_heuristic(x):
         raise ValueError("Shape of x has to be either (n,) or (n, d)")
 
 class SVGD():
-    def __init__(self, dist, n_iter_max, adaptive_kernel=False, get_bandwidth=None, particle_shape=None, adagrad=False):
+    def __init__(self, dist, n_iter_max, adaptive_kernel=False, get_bandwidth=None, particle_shape=None, adagrad=False, noise=0):
         """
         Arguments:
         * dist: instance of class metrics.Distribution. Alternatively, a callable that computes log(p(x))
         * n_iter_max: integer
         * adaptive_kernel: bool
         * get_bandwidth: callable or None
+        * noise: scalar. level of gaussian noise added at each iteration.
         """
         if adaptive_kernel:
             if get_bandwidth is None:
@@ -115,11 +143,12 @@ class SVGD():
         self.adaptive_kernel = adaptive_kernel
         self.adagrad = adagrad
 
-        # these don't trigger recompilation:
+        # these don't trigger recompilation (never used as context in jitted functions)
         self.rkey = random.PRNGKey(0)
 
         # these don't trigger recompilation, but also the compiled method doesn't noticed they changed.
         self.ksd_kernel_range = np.array([0.1, 1, 10])
+        self.noise = noise
 
     def newkey(self):
         """Not pure"""
@@ -163,8 +192,7 @@ class SVGD():
             x = update(x, self.logp, stepsize, _bandwidth, None, self.adagrad, historical_grad)
 
             rkey = random.split(rkey)[0]
-            eps = 10e-3
-            x = x + random.normal(rkey, shape=x.shape) * eps
+            x = x + random.normal(rkey, shape=x.shape) * self.noise
 
             return [x, log, historical_grad, rkey]
 
@@ -173,7 +201,7 @@ class SVGD():
 
     svgd = utils.verbose_jit(svgd, static_argnums=0)
 
-    def step(self, x, logh, lr, svgd_stepsize, ksd_logh=1, gradient_clip_threshold=100):
+    def step(self, rkey, x, logh, lr, svgd_stepsize, ksd_logh=1, gradient_clip_threshold=100):
         """SGD update step
         1) compute SVGD step (forward)
         2) compute Loss (backward)
@@ -185,6 +213,7 @@ class SVGD():
             bandwidth = np.exp(logh)
             ksd_bandwidth = np.exp(ksd_logh)
             xout = update(x, self.logp, svgd_stepsize, bandwidth, None, False, None)
+            xout = xout + random.normal(rkey, shape=x.shape) * self.noise
             return metrics.ksd(xout, self.logp, ksd_bandwidth)
 
         current_loss = loss(logh)
@@ -202,23 +231,19 @@ class SVGD():
     def train(self, rkey, bandwidth, lr, svgd_stepsize, n_steps, ksd_bandwidth=None, update_after=25):
         bandwidth = np.array(bandwidth, dtype=np.float32) # cast to float so it works with jacfwd
         logh = np.log(bandwidth)
-        ksd_logh = np.log(ksd_bandwidth)
 
         x = self.initialize(rkey)
-        rkey = random.split(rkey)[0]
-        x_step = self.initialize(rkey) # use for computing gradient of h
-
-        log = log_step = metrics.initialize_log(self)
-        log["x0"], log_step["x0"] = x_step, x
+        log = metrics.initialize_log(self)
+        log["x0"] = x
         loss = []
-        if ksd_logh is None:
+        if ksd_bandwidth is None:
             concurrent = True
         else:
+            ksd_logh = np.log(ksd_bandwidth)
             concurrent = False
 
         historical_grad = np.zeros(shape=self.particle_shape)
         for i in tqdm(range(n_steps)):
-            # decide whether to update logh in this iteration
             update_logh = i > update_after
 
             if update_logh:
@@ -226,31 +251,24 @@ class SVGD():
                 if concurrent:
                     ksd_logh=logh
 
-                logh, loss_log = self.step(x_step, logh, lr, svgd_stepsize, ksd_logh)
+                rkey = random.split(rkey)[0]
+                logh, loss_log = self.step(rkey, x, logh, lr, svgd_stepsize, ksd_logh)
                 loss.append(loss_log["loss"])
 
             bandwidth = np.exp(logh)
-            log      = metrics.update_log(self, i, x,      log,      bandwidth)
-            log_step = metrics.update_log(self, i, x_step, log_step, bandwidth)
+            log = metrics.update_log(self, i, x, log, bandwidth)
 
-            x      = update(x,      self.logp, svgd_stepsize, bandwidth, x,      self.adagrad, historical_grad)
-            x_step = update(x_step, self.logp, svgd_stepsize, bandwidth, x_step, self.adagrad, historical_grad)
+            x = update(x, self.logp, svgd_stepsize, bandwidth, x, self.adagrad, historical_grad)
+            rkey = random.split(rkey)[0]
+            x = x + random.normal(rkey, shape=x.shape) * self.noise
 
             if np.any(np.isnan(logh)):
                 warnings.warn(f"NaNs detected in logh at iteration {i}. Training interrupted.", RuntimeWarning)
                 break
-
-            for xs in (x, x_step):
-                if np.any(np.isnan(xs)):
-                    warnings.warn(f"NaNs detected in x at iteration {i}. Logh is fine, which means NaNs come from update. Training interrupted.", RuntimeWarning)
-                    break
-            else:
-                continue
-            break
-
-        return x, x_step, log, log_step, loss #x_grad, x_step, log_grad, log_svgd, loss
-
-
+            elif np.any(np.isnan(x)):
+                warnings.warn(f"NaNs detected in x at iteration {i}. Logh is fine, which means NaNs come from update. Training interrupted.", RuntimeWarning)
+                break
+        return x, log, loss #x_grad, x, log_grad, log_svgd, loss
 
 
 

utils.py
@@ -54,7 +54,7 @@ def get_ada_loss(m):
 
 
 
-# wrapper that prints when the function compiles 
+# wrapper that prints when the function compiles
 def verbose_jit(fun, *jargs, **jkwargs):
     """Does same thing as jax.jit, only that it also inserts a print statement."""
     @wraps(fun)
@@ -154,14 +154,16 @@ def ard(x, y, h):
     OUT:
     scalar kernel(x, y, h).
     """
-    x, y = np.array([x, y])
-    assert x.ndim <= 1
-    assert y.ndim <= 1
+    x, y = np.array(x), np.array(y)
+    if x.shape != y.shape:
+        raise ValueError(f"Shapes of particles x and y need to match. Recieved shapes x: {x.shape}, y: {y.shape}")
+    if x.ndim > 1:
+        raise ValueError(f"Input particles x and y can't have more than one dimension. Instead they have rank {x.ndim}")
 
     h = np.array(h)
-    assert h.ndim == 1 or h.ndim == 0
-    assert x.shape == y.shape
-    if h.ndim == 1:
+    if h.ndim > 1:
+        raise ValueError(f"Bandwidth can't have more than one dimension. Instead h has rank {h.ndim}")
+    elif h.ndim == 1:
         assert x.shape == h.shape
     return np.exp(- np.sum((x - y)**2 / h**2) / 2)
 
@@ -233,19 +235,26 @@ def cartesian_product(*arrays):
 def dict_concatenate(dict_list):
     """
     Arguments:
-    * dict_list: a list of dictionaries with the same keys. All values must be numeric.
+    * dict_list: a list of dictionaries with the same keys. All values must be numeric or a nested dict.
 
     Returns:
     * a dictionary with the same keys as the input dictionaries. The values are np
     arrays consisting of the concatenation of the values in the input dictionaries.
     """
-    assert all([dict_list[i].keys() == dict_list[i+1].keys() for i in range(len(dict_list)-1)])
+    for d in dict_list:
+        if type(d) is not dict:
+            raise TypeError("Input has to be a list consisting of dictionaries.")
+        elif not all([dict_list[i].keys() == dict_list[i+1].keys() for i in range(len(dict_list)-1)]):
+            raise ValueError("The keys of all input dictionaries need to match.")
 
     keys = dict_list[0].keys()
     out = {key: [d[key] for d in dict_list] for key in keys}
 
     for k, v in out.items():
-        out[k] = np.array(v)
+        try:
+            out[k] = np.array(v)
+        except TypeError:
+            out[k] = dict_concatenate(v)
 
     return out
 
@@ -260,7 +269,18 @@ def dict_mean(dict_list):
     """
     out = dict_concatenate(dict_list)
     for k, v in out.items():
-        out[k] = np.mean(v, axis = 0)
-        assert out[k].shape == dict_list[0][k].shape
-
+        try:
+            out[k] = np.mean(v, axis = 0)
+            assert out[k].shape == dict_list[0][k].shape
+        except TypeError:
+            out[k] = dict_mean(v)
     return out
+
+def dict_divide(da, db):
+    """divide numeric dict recursively, a / b."""
+    for (k, a), (k, b) in zip(da.items(), db.items()):
+        try:
+            da[k] = a / b
+        except TypeError:
+            da[k] = dict_divide(a, b)
+    return da