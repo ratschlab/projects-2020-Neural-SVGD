Fixed minor bugs in metrics and stein.py. Modifies SVGD.train to normalize bandwidth gradients.

metrics.py
@@ -85,10 +85,16 @@ class Gaussian(Distribution):
         """self.expectations is a list of expected values of the following expressions: x, x^2, cos(x), sin(x)"""
         mean = np.array(mean)
         cov = np.array(cov)
+        if cov.ndim == 0:
+            if mean.ndim == 1: # d fixed by shape of mean
+                cov = len(mean) * (cov,)
+                cov = np.array(cov)
+            elif mean.ndim == 0:
+                cov = cov[np.newaxis, np.newaxis]
+            else:
+                raise ValueError(f"Recieved inappropriate shape {mean.shape} for mean. (Wrong nr of dimensions).")
         if cov.ndim == 1:
             cov = np.diag(cov)
-        elif cov.ndim == 0:
-            cov = cov[np.newaxis, np.newaxis]
         if mean.ndim == 0:
             mean = np.ones(len(cov)) * mean
         assert mean.ndim == 1 and cov.ndim == 2

stein.py
@@ -18,28 +18,29 @@ def stein_operator(fun, x, logp, transposed=False):
     Returns:
     Stein operator $\mathcal A$ evaluated at fun and x:
     \[ \mathcal A_p [\text{fun}](x) .\]
-    np.array of shape (d,) if transposed else (d, d) # TODO check if shapes are correct
+    This expression takes the form of a scalar if transposed else a dxd matrix
     """
     x = np.array(x, dtype=np.float32)
     if x.ndim != 1:
         raise ValueError(f"x needs to be an np.array of shape (d,). Instead, x has shape {x.shape}")
+    fx = fun(x)
     if transposed:
-        out = stein_operator(fun, x, logp, transposed=False)
-        if out.ndim == 1 and x.shape[0] != 1:
-            raise ValueError(f"Got passed transposed = True, but the input function {fun.__name__} returns a scalar. This doesn't make sense: the transposed Stein operator acts on functions in R^d.")
-        elif out.ndim == 1:
-            return out
-        elif out.ndim == 2:
-            return np.trace(out)
+        if fx.ndim == 0:   # f: R^d --> R
+            raise ValueError(f"Got passed transposed = True, but the input function {fun.__name__} returns a scalar. This doesn't make sense: the transposed Stein operator acts only on vector-valued functions.")
+        elif fx.ndim == 1: # f: R^d --> R^d
+            return np.inner(grad(logp)(x), fun(x)) + np.trace(jacfwd(fun)(x).transpose())
         else:
-            raise ValueError("Output of stein_operator must have shape (d,) or (d, d)")
+            raise ValueError(f"Output of input function {fun.__name__} needs to have dimension 1 or two. Instead got output of shape {fx.shape}")
     else:
-        fx = fun(x)
         if fx.ndim == 0:   # f: R^d --> R
             return grad(logp)(x) * fun(x) + grad(fun)(x)
         elif fx.ndim == 1: # f: R^d --> R^d
-            return np.einsum("i,j->ij", grad(logp)(x), fun(x)) + jacfwd(fun)(x)
+            return np.einsum("i,j->ij", grad(logp)(x), fun(x)) + jacfwd(fun)(x).transpose()
+        elif fx.ndim == 2 and fx.shape[0] == fx.shape[1]:
+            raise NotImplementedError()
+#            return np.einsum("ij,j->ij", fun(x), grad(logp)(x)) + #np.einsum("iii->i", jacfwd(fun)(x).transpose())
         else:
+            raise ValueError(f"Output of input function {fun.__name__} needs to be a scalar, a vector, or a square matrix. Instead got output of shape {fx.shape}")
             raise ValueError()
 
 def stein(fun, xs, logp, transposed=False):

svgd.py
@@ -173,7 +173,17 @@ class SVGD():
         * stepsize: scalar"""
         def ksd_sq(h):
             return metrics.ksd_squared(x, self.logp, h)
-        return h + stepsize * optimizers.clip_grads(grad(ksd_sq)(h), 0.00005)
+
+        # normalize gradient
+        gradient = grad(ksd_sq)(h)
+        gradient = gradient / np.linalg.norm(gradient)
+
+#        # clip gradient
+#        gradient = grad(ksd_sq)(h)
+#        gradient = optimizers.clip_grads(gradient, 0.00005)
+
+        return h + stepsize * gradient
+
     kernel_step = jit(kernel_step, static_argnums=0)
 
     def svgd_step(self, x, h, stepsize):
@@ -196,18 +206,25 @@ class SVGD():
         ksd_post = []
         for i in tqdm(range(n_steps)):
             ksd_pre.append(metrics.ksd_squared(x, self.logp, bandwidth))
+
+            # update bandwidth
             bandwidth = self.kernel_step(bandwidth, x, lr)
             ksd_post.append(metrics.ksd_squared(x, self.logp, bandwidth))
-            x = self.svgd_step(x, bandwidth, svgd_stepsize)
-            log = metrics.update_log(self, i, x, log, bandwidth)
-
             if np.any(np.isnan(bandwidth)):
+                log["interrupt_iter"] = i
+                log["last_bandwidth"] = log["desc"]["bandwidth"][i-1]
                 warnings.warn(f"NaNs detected in bandwidth at iteration {i}. Training interrupted.", RuntimeWarning)
                 break
-            elif np.any(np.isnan(x)):
+
+            # update x
+            x = self.svgd_step(x, bandwidth, svgd_stepsize)
+            log = metrics.update_log(self, i, x, log, bandwidth)
+            if np.any(np.isnan(x)):
+                log["interrupt_iter"] = i
                 warnings.warn(f"NaNs detected in x at iteration {i}. Logh is fine, which means NaNs come from update. Training interrupted.", RuntimeWarning)
                 break
         log["metric_names"] = self.dist.metric_names
         log["ksd_pre"] = np.array(ksd_pre)
         log["ksd_post"] = np.array(ksd_post)
+
         return x, log