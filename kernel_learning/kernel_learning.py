Fixed bias in KSD variance estimator

kernel_learning/kernel_learning.py
@@ -17,7 +17,6 @@ import metrics
 import stein
 import kernels
 import nets
-import svgd
 import distributions
 
 import os
@@ -56,6 +55,7 @@ class KernelLearner():
         self.activation_kernel = activation_kernel
         self.target = target
         self.lambda_reg = lambda_reg
+        self.normalize = normalize
         self.threadkey, subkey = random.split(key)
 
         # net and optimizer
@@ -63,7 +63,7 @@ class KernelLearner():
                                   with_bias=False)
         self.decoder = nets.build_mlp(self.sizes, name="decoder",
                                       skip_connection=False, with_bias=False)
-        self.opt = svgd.Optimizer(*optimizers.rmsprop(learning_rate))
+        self.opt = Optimizer(*optimizers.rmsprop(learning_rate))
         self.step_counter = 0
         self.initialize_optimizer(subkey)
         self.rundata = {}
@@ -97,7 +97,7 @@ class KernelLearner():
     def loss_fn(self, params, samples):
         encoder_params, decoder_params = params
         kernel = self.get_kernel(params)
-        ksd, var = stein.ksd_squared_v(samples, self.target.logpdf, kernel, True)
+        ksd, var = stein.ksd_squared_u(samples, self.target.logpdf, kernel, True)
         def enc(x): return self.mlp.apply(    encoder_params, None, x)
         def dec(z): return self.decoder.apply(decoder_params, None, z)
         def autoencoder_loss(x): return np.linalg.norm(x - dec(enc(x)))**2
@@ -330,7 +330,8 @@ class AdversarialSVGD():
                  svgd_lr=0.1,
                  kernel_lr=0.1,
                  lambda_reg=0.1,
-                 svgd_key=None):
+                 svgd_key=None,
+                 normalize=False):
         """
         Initialize containers for kernel learning and particle updates.
         """
@@ -340,7 +341,8 @@ class AdversarialSVGD():
                                     sizes,
                                     kernels.get_rbf_kernel(1),
                                     kernel_lr,
-                                    lambda_reg)
+                                    lambda_reg,
+                                    normalize=normalize)
         if svgd_key is not None:
             keyb = svgd_key
         self.svgd = SVGD(key=keyb,

kernel_learning/stein.py
@@ -2,7 +2,10 @@ from functools import partial
 import jax.numpy as np
 from jax import grad, vmap, random, jacfwd, jit
 from jax.ops import index_update, index
+
 import variance
+import distributions
+import kernels
 
 def stein_operator(fun, x, logp, transposed=False, aux=False):
     """
@@ -244,14 +247,29 @@ def ksd_squared_l(samples, logp, k, return_variance=False):
     else:
         return np.mean(outs)
 
-@partial(jit, static_argnums=(2,3))
-def h_var(xs, ys, logp, k):
-    """Estimate for Var(h(X, Y))
-    Recall Var(KSD_L) = 1/n Var(h(X, Y))"""
-    def h(x, y):
-        """x, y: np.arrays of shape (d,)"""
-        def inner(x):
-            kx = lambda y_: k(x, y_)
-            return stein_operator(kx, y, logp)
-        return stein_operator(inner, x, logp, transposed=True)
-    return np.var(vmap(h)(xs, ys), ddof=1) # unbiased variance
+def h(x, y, kernel, logp):
+    k=kernel
+    def h2(x_, y_): return np.inner(grad(logp)(y_), grad(k, argnums=0)(x_, y_))
+    def d_xk(x_, y_): return grad(k, argnums=0)(x_, y_)
+    out = np.inner(grad(logp)(x), grad(logp)(y)) * k(x,y) +\
+            h2(x, y) + h2(y, x) +\
+            np.trace(jacfwd(d_xk, argnums=1)(x, y))
+    return out
+
+def g(x, y, kernel, logp):
+    """x, y: np.arrays of shape (d,)"""
+    k=kernel
+    def inner(x):
+        kx = lambda y_: k(x, y_)
+        return stein_operator(kx, y, logp)
+    return stein_operator(inner, x, logp, transposed=True)
+
+def test_h_successful():
+    target = distributions.Gaussian(0, 5)
+    source = distributions.Gaussian(3, 1)
+    k = kernels.get_rbf_kernel_logscaled(logh=0)
+    logp = target.logpdf
+    x, y = source.sample(2)
+    return g(x, y, k, logp) == h(x, y, k, logp)
+
+assert test_h_successful()

kernel_learning/variance.py
@@ -3,9 +3,12 @@ import jax.numpy as np
 from jax import grad, vmap, random, jacfwd, jit
 from jax.ops import index_update, index
 
+import utils
+
 def var_exp(gram):
     """
-    Estimator for $Var_Y(E_X[h(X, Y)])$, where h is a symmetric kernel function.
+    Estimator for $Var_Y(E_X[h(X, Y)])$, where h is a symmetric kernel function,
+    and X, Y are iid.
     For background, see this technical report: https://arxiv.org/pdf/1906.02104.pdf
 
     Args
@@ -17,13 +20,20 @@ def var_exp(gram):
     n = gram.shape[0]
     diagonal_indices = [list(range(n))]*2
     gramzero = index_update(gram, diagonal_indices, 0)
-    return (np.linalg.norm(np.dot(gramzero, np.ones(n)))**2 \
-            - np.linalg.norm(gramzero)**2) / (n * (n-1) * (n-2))
+    del gram
+    one_n = np.ones(n)
+    expectation_of_square = (np.linalg.norm(np.dot(gramzero, one_n))**2 \
+                             - np.linalg.norm(gramzero)**2) / (n * (n-1) * (n-2))
+    square_of_expectation = (utils.vmv_dot(one_n, gramzero, one_n)**2 \
+                             - 4 * np.linalg.norm(np.dot(gramzero, one_n))**2 \
+                             + 2 * np.linalg.norm(gramzero)**2) / (n*(n-1)*(n-1)*(n-3))
+    return expectation_of_square - square_of_expectation
 
 
 def var_hxy(gram):
     """
-    Estimator for $Var_{XY}(h(X, Y))$, where h is a symmetric kernel function.
+    Estimator for $Var_{XY}(h(X, Y))$, where h is a symmetric kernel function,
+    and X, Y are iid.
     For background, see this technical report: https://arxiv.org/pdf/1906.02104.pdf
 
     Args
@@ -45,7 +55,7 @@ def var_hxy(gram):
 
 def var_ksd(xs, logp: callable, k: callable):
     """
-    Estimator for $Var_{XY} \hat KSD$, where \hat KSD is a U-estimator for the 
+    Estimator for $Var_{XY} \hat KSD$, where \hat KSD is a U-estimator for the
     kernelized Stein discrepancy with kernel k.
     For background, see this technical report: https://arxiv.org/pdf/1906.02104.pdf
 