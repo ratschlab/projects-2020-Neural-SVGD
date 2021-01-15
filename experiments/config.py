066729caa46d16a02f3663208bfdf7a31347fcf4
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2021-01-15 20:32:30 +0100

    fix results path

--- experiments/config.py
+++ experiments/config.py
@@ -1,3 +1,8 @@
-#figure_path = "/home/lauro/documents/msc-thesis/paper/latex/figures/"
-figure_path = "./experiments/results/figures/"
-results_path = "./experiments/results/"
+import os
+on_cluster = not os.getenv("HOME") == "/home/lauro"
+if on_cluster:
+    results_path = "/cluster/home/dlauro/projects-2020-Neural-SVGD/experiments/results/"
+else:
+    results_path = "/home/lauro/code/msc-thesis/main/experiments/results/"
+figure_path = results_path + "figures/"
+# figure_path = "/home/lauro/documents/msc-thesis/paper/latex/figures/"
