36f1817324c7513247468088d72bbf3f03f4c95b
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2021-01-28 00:51:48 +0100

    decrease batch size

--- experiments/config.py
+++ experiments/config.py
@@ -2,13 +2,13 @@ import os
 on_cluster = not os.getenv("HOME") == "/home/lauro"
 if on_cluster:
     results_path = "/cluster/home/dlauro/projects-2020-Neural-SVGD/experiments/results/"
-    batch_size = 1024
+    batch_size = 128
     model_size = 'large'
     data_dir = "./data"
 else:
     results_path = "/home/lauro/code/msc-thesis/main/experiments/results/"
     batch_size = 128
-    model_size = 'small'
+    model_size = 'large'
     data_dir = "/tmp/tfds"
 figure_path = results_path + "figures/"
 n_samples = 100
